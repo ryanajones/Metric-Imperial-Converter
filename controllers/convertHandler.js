f7fcf4f657212cb31e7c770dea9331ca01410bf8
Ryan Jones <ryanajonesdev@gmail.com>
2020-11-21 04:04:00 -0800

    initial commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,52 +1,50 @@
 /*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the handler logic below
+ *
+ *
+ */
 
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
+  this.getNum = function (input) {
+    let result;
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
+  this.getUnit = function (input) {
+    let result;
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let result;
+
+    return result;
+  };
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
+    let result;
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    let result;
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
